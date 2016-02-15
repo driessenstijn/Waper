0f1a6a780a60aba6806f1139ba41d650979bf592
Michelangelo <dragonbe+github@gmail.com>
2016-02-15 20:03:24 +0100

    Your wish is my command, oh divined supreme yourhighness

--- Example/loadup.php
+++ Example/loadup.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Waper\Waper;
 
-$waper = new Waper('https://twitter.com');
+$waper = new Waper('http://hourpendulum.com');
 $img = $waper->fetch();
 
 var_dump($img);
